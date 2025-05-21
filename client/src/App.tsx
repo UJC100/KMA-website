0170629edb539021f46f230de1ff7d0fe216fd07
UJC100 <meetjohnchima@gmail.com>
2025-05-21 22:29:49 +0800

    added icons

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,16 @@
-import { useState } from 'react'
-import './App.css'
+// import { useState } from 'react'
+import Navbar from './component/Navbar'
+import { useLocation } from 'react-router-dom'
 
 function App() {
-  
+  const isOwnerPath = useLocation().pathname.includes('owner')
+
   return (
-    <>
+    <div>
+
+      {!isOwnerPath && <Navbar/>}
       
-    </>
+    </div>
   )
 }
 
