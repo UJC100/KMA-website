a5b842ebd204a391af45f39e107528c7f1b7bde8
UJC100 <meetjohnchima@gmail.com>
2025-05-27 22:18:11 +0800

    quick fix

---
+++ client/src/component/MeetOurMentors.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const MeetOurMentors = () => {
+
+
+  return (
+        <div className='flex flex-col items-center justify-center px-6 md:px-16 lg:px-24 xl:px-32  gap-3 py-20'>
+
+        </div>
+    );
+};
+
+export default MeetOurMentors;
