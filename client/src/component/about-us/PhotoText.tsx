e1d290e6b3de0b93617b4f8a5c4aee1cf18bf500
UJC100 <meetjohnchima@gmail.com>
2025-06-18 00:24:50 +0800

    quicl fix

--- client/src/component/about-us/PhotoText.tsx
+++ client/src/component/about-us/PhotoText.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import AnimateOnScroll from '../AnimationOnScroll';
 
 
 interface HeroSectionProps {
@@ -15,10 +16,13 @@ const PhotoText = ({flip = false, image, title, description}: HeroSectionProps) 
         flip ? 'md:flex-row-reverse' : ''
       }`}>
         <img src={image} alt={title} className='object-cover md:w-1/2 h-[700px] bg-black/50' />
-         <div className="flex flex-col justify-center items-start md:text-left max-w-1/2 px-10">
+         <AnimateOnScroll
+          direction="zoom"
+          scaleLevel={0.7}
+          className="flex flex-col justify-center items-start md:text-left max-w-1/2 px-10">
         <h1 className="text-6xl mb-8 font-light">{title}</h1>
         <p className="text-lg font-extralight ">{description}</p>
-      </div>
+      </AnimateOnScroll>
         </div>
     );
 };
