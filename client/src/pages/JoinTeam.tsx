fe6aaf9a73e696113ca3ecfb20647350eb857a17
UJC100 <meetjohnchima@gmail.com>
2025-07-02 02:40:20 +0800

    implemented google meet scheduler

--- client/src/pages/JoinTeam.tsx
+++ client/src/pages/JoinTeam.tsx
@@ -1,89 +1,197 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
-import Calendar from 'react-calendar';
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 const JoinTeam = () => {
-  const [selectedDate, setSelectedDate] = useState(null);
-  const [menteeName, setMenteeName] = useState('');
-  const [menteeEmail, setMenteeEmail] = useState('');
-const availableDates = ['2025-06-25', '2025-06-26', '2025-06-28'];
+  type FormValues = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  date: string;
+  time: string;
+  country: string;
+};
+
+
+  const initialFormValues = {firstName: "", lastName: "", email: "", time: '', date: '', country: ""}
+  const [formData, setFormData] = useState(initialFormValues)
+  const [error, setError] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false)
+  
+  const availableDates = [
+    "2025-07-02",
+    "2025-07-03",
+    "2025-07-04",
+    "2025-07-08",
+  ];
+  const availableTimes = [
+    "12:00 PM",
+    "12:30 PM",
+    "01:00 PM",
+    "01:30 PM",
+    "02:00 PM",
+    "02:30 PM",
+    "03:00 PM",
+    "03:30 PM",
+    "04:00 PM",
+    "04:30 PM",
+    "05:00 PM",
+  ];
+  const minDate = new Date(
+    Math.min(...availableDates.map((d) => new Date(d).getTime()))
+  );
+  const maxDate = new Date(
+    Math.max(...availableDates.map((d) => new Date(d).getTime()))
+  );
 
   const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
+ 
+
+  const handleChange = (e) => {
+    const {name, value} = e.target
+    setFormData({...formData, [name]: value})
+  }
+
+  const handleDateChange = (date) => {
+    setFormData({...formData, date: date.toLocaleDateString("en-CA")})
+  }
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // Optional: Save form data to backend or localStorage
-    navigate("/payment");
+
+    const validationErrors = validate(formData)
+    setError(validate(formData))
+    setIsSubmit(true)
+
+
+  if (Object.keys(validationErrors).length > 0) return; 
+
+     const res = await fetch('/api/book-meeting', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(formData),
+  });
+
+  const data = await res.json();
+  console.log(data)
+    // navigate("/payment");
   };
+  useEffect(()=> {
+    if(Object.keys(error).length === 0 && isSubmit) {
+      console.log(formData)
+    }
+  }, [error, formData, isSubmit])
+
+  const validate = (values: FormValues) => {
+    const errors: Partial<FormValues> = {}
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!values.firstName){
+      errors.firstName = "First Name is Required"
+    }
+    if (!values.lastName){
+      errors.lastName = "Last Name is Required"
+    }
+
+    if (!values.email){
+      errors.email = "Email is Required"
+    } else if (!regex.test(values.email)) {
+    errors.email = "Invalid email format";
+    }
+
+    if (!values.date){
+      errors.date = "Date is Required"
+    }
+    if (!values.time){
+      errors.time = "Time is Required"
+    }
+    if (!values.country){
+      errors.country = "Country is Required"
+    }
+
+    return errors
+  }
 
   return (
     <div className="flex flex-col md:flex-row h-screen">
       <div className="w-1/2">
-      <img src={assets.logo} className="w-30 h-30" alt="logo" />
+        <img src={assets.logo} className="w-30 h-30" alt="logo" />
 
-        <form
-          onSubmit={handleSubmit}
-          className="px-10 py-14 space-y-7"
-        >
-          <h2 className="text-5xl md:text-4xl mb-8 font-medium text-gray-800">Join Our Mentee Program</h2>
+        <form onSubmit={handleSubmit} className="px-10 py-14 space-y-7">
+          <h2 className="text-5xl md:text-4xl mb-8 font-medium text-gray-800">
+            Join Our Mentee Program
+          </h2>
 
-         <div className="flex gap-4">
+          <div className="flex gap-4">
+            <input
+            name="firstName"
+              type="text"
+              placeholder="First name"
+              value={formData.firstName}
+              onChange={handleChange}
+              className="flex-1 border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring focus:border-blue-500"
+            />
+            
+            <input
+              name="lastName"
+              type="text"
+              placeholder="Last name"
+              value={formData.lastName}
+              onChange={handleChange}
+              className="flex-1 border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring focus:border-blue-500"
+            />
+          </div>
+
+
+          {/* Country */}
+          <select name="country" value={formData.country} onChange={handleChange} className="w-full border border-gray-300 px-4 py-2 rounded-md text-black/50">
+            <option>Choose a country where you reside</option>
+            <option>Australia</option>
+            <option>Nigeria</option>
+            <option>Philippines</option>
+            <option>Others</option>
+          </select>
+
+          {/* Email */}
           <input
-            type="text"
-            placeholder="First name"
-            className="flex-1 border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring focus:border-blue-500"
+            onChange={handleChange}
+            type="email"
+            name="email"
+            placeholder="Email address"
+            value={formData.email}
+            className="w-full border border-gray-300 px-4 py-2 rounded-md"
           />
-          <input
-            type="text"
-            placeholder="Last name"
-            className="flex-1 border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring focus:border-blue-500"
+
+          <select
+            id="time"
+            name="time"
+            value={formData.time}
+            onChange={handleChange}
+            className="w-full border border-gray-300 px-4 py-2 rounded text-black/50"
+          >
+            <option value="" >-- Select a time --</option>
+            {availableTimes.map((time) => (
+              <option key={time} value={time}>
+                {time}
+              </option>
+            ))}
+          </select>
+
+          <Calendar
+            onClickDay={handleDateChange}
+            value={formData.date ? new Date(formData.date) : null}
+            minDate={minDate}
+            maxDate={maxDate}
+            activeStartDate={minDate}
+            tileDisabled={({ date }) => {
+              const formatted = date.toLocaleDateString("en-CA");
+              const isPast = date.getTime() < new Date().setHours(0, 0, 0, 0);
+              const isUnavailable = !availableDates.includes(formatted);
+              return isPast || isUnavailable;
+            }}
           />
-        </div>
-
-        {/* Address */}
-        <input
-          type="text"
-          placeholder="Street address"
-          className="w-full border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring focus:border-blue-500"
-        />
-
-        {/* City, State, Zip */}
-        <div className="flex gap-4">
-          <input
-            type="text"
-            placeholder="City"
-            className="flex-1 border border-gray-300 px-4 py-2 rounded-md"
-          />
-          <input
-            type="text"
-            placeholder="State"
-            className="w-1/3 border border-gray-300 px-4 py-2 rounded-md"
-          />
-          <input
-            type="text"
-            placeholder="Postcode"
-            className="w-1/3 border border-gray-300 px-4 py-2 rounded-md"
-          />
-        </div>
-
-        {/* Country */}
-        <select className="w-full border border-gray-300 px-4 py-2 rounded-md text-gray-600">
-          <option>Choose a country</option>
-          <option>Australia</option>
-          <option>Nigeria</option>
-          <option>Philippines</option>
-          <option>Others</option>
-        </select>
-
-        {/* Email */}
-        <input
-          type="email"
-          placeholder="Email address"
-          className="w-full border border-gray-300 px-4 py-2 rounded-md"
-        />
-          
-
           <button
             type="submit"
             className="w-full bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 transition"
@@ -91,24 +199,9 @@ const availableDates = ['2025-06-25', '2025-06-26', '2025-06-28'];
             Proceed to Payment
           </button>
         </form>
-         <div className="border rounded-lg p-4 shadow-sm mb-4">
-        <Calendar
-  onClickDay={setSelectedDate}
-  value={selectedDate}
-  tileDisabled={({ date }) => {
-    const today = new Date();
-    const formatted = date.toISOString().split('T')[0];
-    const isPast = date.getTime() < today.setHours(0, 0, 0, 0);
 
-    const isUnavailable = !availableDates.includes(formatted);
-    return isPast || isUnavailable;
-  }}
-  className="rounded border"
-/>
       </div>
-      </div>
-      <div className="bg-amber-500 w-1/2 m-5 rounded-2xl">
-      </div>
+      <div className="bg-amber-500 w-1/2 m-5 rounded-2xl"></div>
     </div>
   );
 };
