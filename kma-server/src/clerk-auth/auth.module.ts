8c60d3c80501e00d2b30a3d33fc67ca8afa8f231
UJC100 <meetjohnchima@gmail.com>
2025-08-29 18:27:45 +0800

    fixed path issue

--- kma-server/src/clerk-auth/auth.module.ts
+++ kma-server/src/clerk-auth/auth.module.ts
@@ -3,8 +3,8 @@ import { ClerkStrategy } from './clerk.strategy';
 import { PassportModule } from '@nestjs/passport';
 
 import { ConfigModule } from '@nestjs/config';
-import { ClerkClientProvider } from 'src/providers/clerk-client.providers';
-import { UsersModule } from 'src/modules/v1/users/users.module';
+import { ClerkClientProvider } from '../providers/clerk-client.providers';
+import { UsersModule } from '../modules/v1/users/users.module';
 
 @Module({
   imports: [PassportModule, ConfigModule, UsersModule],
